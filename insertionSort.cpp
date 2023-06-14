b31301f3c5a7188a147c7c442101e7e5c3d42260
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2023-06-14 15:58:15 +0530

    update-14-Jun

---
+++ insertionSort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
